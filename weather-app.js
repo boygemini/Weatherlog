c29962897fa8a1300ec5ec3dcf1060db1f739d43
Jubril Bolajoko Gbadebo <gbadebojubril@gmail.com>
2022-06-30 04:43:46 -0500

    new commit

--- weather-app.js
+++ weather-app.js
@@ -53,7 +53,7 @@ document.getElementById("userloc").addEventListener("click", (e) => {
     let Loc = `${position.coords.longitude},${position.coords.longitude}`;
     loadupWeather(Loc);
 
-    const apiiUrl = "http://api.weatherapi.com/v1/forecast.json?q=";
+    const apiiUrl = "https://api.weatherapi.com/v1/forecast.json?q=";
     const apiiKey = "3b8b8609054e425bbdc33941220106";
     let newReq = new XMLHttpRequest();
     newReq.open("GET", `${apiiUrl}${Loc}&key=${apiiKey}`, false);
@@ -96,7 +96,7 @@ document.getElementById("userloc").addEventListener("click", (e) => {
 // }
 
 function citiesTimeCollector(city, ID, indicator) {
-  const apiUrll = "http://api.weatherapi.com/v1/forecast.json?q=";
+  const apiUrll = "https://api.weatherapi.com/v1/forecast.json?q=";
   const apiKeyy = "3b8b8609054e425bbdc33941220106";
   // const apiUrl = 'http://127.0.0.1:5500./testapi.json';
   let timereq = new XMLHttpRequest();
@@ -225,7 +225,7 @@ const loadupWeather = (city_selected) => {
 
   async function timeoutHandler() {
     await sleep(1);
-    const apiUrl = "http://api.weatherapi.com/v1/forecast.json?q=";
+    const apiUrl = "https://api.weatherapi.com/v1/forecast.json?q=";
     const apiKey = "3b8b8609054e425bbdc33941220106";
 
     // const apiUrl = 'http://127.0.0.1:5500./testapi.json';
@@ -879,7 +879,7 @@ const loadupWeather = (city_selected) => {
           }
 
           function switcher() {
-            const apiUrl = "http://api.weatherapi.com/v1/forecast.json?q=";
+            const apiUrl = "https://api.weatherapi.com/v1/forecast.json?q=";
             const apiKey = "3b8b8609054e425bbdc33941220106";
 
             // const apiUrl = 'http://127.0.0.1:5500./testapi.json';
